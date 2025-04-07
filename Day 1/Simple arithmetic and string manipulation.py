ce95192459cbb5ec857943225c7ffbec1d8bc2f2
VeenusDadri <162694506+VeenusDadri@users.noreply.github.com>
2025-04-07 17:10:57 +0530

    Add files via upload

---
+++ Day 1/Simple arithmetic and string manipulation.py
@@ -0,0 +1,26 @@
+# Assigning variables and determining their type
+x=5
+print ( f"type of {x} is :")
+print(type(x))
+
+
+f=5.5
+print ( f"type of {f} is :")
+print(type(f))
+
+
+name ="Veenus"
+print ( f"type of {name} is :")
+print(type(name))
+
+company = 'InfoObjects'
+print ( f"type of {company} is :")
+print(type (company))
+
+is_company = True
+print ( f"type of {is_company} is :")
+print(type (is_company))
+
+training_subject = [ 'python' , 'pyspark' , 'Databricks']
+print ( f"type of {training_subject} is :")
+print (type (training_subject))
